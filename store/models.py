db14e320f1465d8de8a91277ad60221e19af498e
AChlebba <adam343@protonmail.com>
2022-11-04 00:24:39 +0100

    cart functionality added

--- store/models.py
+++ store/models.py
@@ -4,16 +4,16 @@ from django.urls import reverse
 
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    description = models.TextField(max_length=500, blank=True)
-    price = models.DecimalField(decimal_places=2, max_digits=4)
-    images = models.ImageField(upload_to='photos/products')
-    stock = models.IntegerField()
-    is_available = models.BooleanField(default=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    create_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)
+    product_name    = models.CharField(max_length=200, unique=True)
+    slug            = models.SlugField(max_length=200, unique=True)
+    description     = models.TextField(max_length=500, blank=True)
+    price           = models.DecimalField(decimal_places=2, max_digits=4)
+    images          = models.ImageField(upload_to='photos/products')
+    stock           = models.IntegerField()
+    is_available    = models.BooleanField(default=True)
+    category        = models.ForeignKey(Category, on_delete=models.CASCADE)
+    create_date     = models.DateTimeField(auto_now_add=True)
+    modified_date   = models.DateTimeField(auto_now=True)
 
     def get_url(self):
         return reverse('product_detail', args=[self.category.slug, self.slug])
