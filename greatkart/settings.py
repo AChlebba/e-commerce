a0bdd20490ec45cc265e0db170aa1de836a002e6
AChlebba <adam343@protonmail.com>
2022-11-07 16:27:59 +0100

    account verification added

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -140,3 +140,14 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# SMTP CONFIGURATION
+# mail: ecommerceprojectbot@gmail.com
+# pw: ecommerce@mail
+# app_pw: isknycncvuayvbrt
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ecommerceprojectbot@gmail.com'
+EMAIL_HOST_PASSWORD = 'isknycncvuayvbrt'
+EMAIL_USE_TLS = True
