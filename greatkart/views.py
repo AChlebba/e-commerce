6ebf33d6a7f7649a72fa2503bae303cff67f0afc
AChlebba <adam343@protonmail.com>
2022-11-22 22:58:23 +0100

    More products added

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from store.models import Product
 from store.models import ReviewRating
+from django.db.models import Avg
 
 def home(request):
-    products = Product.objects.all().filter(is_available=True).order_by('-create_date')
+    products = Product.objects.filter(is_available=True).annotate(score=Avg('reviewrating__rating')).order_by('-score')[:4]
 
     for product in products:
         reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+        
 
     context = {
         'products': products,
--- store/models.py
+++ store/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     product_name    = models.CharField(max_length=200, unique=True)
     slug            = models.SlugField(max_length=200, unique=True)
     description     = models.TextField(max_length=500, blank=True)
-    price           = models.DecimalField(decimal_places=2, max_digits=4)
+    price           = models.DecimalField(decimal_places=2, max_digits=5)
     images          = models.ImageField(upload_to='photos/products')
     stock           = models.IntegerField()
     is_available    = models.BooleanField(default=True)
